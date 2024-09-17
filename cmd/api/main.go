7802c441ec67ae320857990e29a121bc65e7cf43
Caio Padilha <caiocfpadilha@gmail.com>
2024-09-17 20:02:44 -0300

    Improve the use of environmental variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"ccesarfp.com/StrangerAir/internal/config"
+	"fmt"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	server := config.NewServer()
+
+	err := server.LoadEnv()
+	if err != nil {
+		log.Fatalf("Error reading config file. Message Error: %s", err)
+	}
+
+	log.Printf("Application initialization took %s", server.GetLifeTime())
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", viper.GetString("SERVER_PORT")), nil))
 }
---
+++ internal/config/server.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"time"
+)
+
+// Server manages the server's start time and environment variables.
+type Server struct {
+	StartedAt time.Time
+}
+
+// NewServer creates a new instance of Server with the start time set to the current time.
+func NewServer() *Server {
+	return &Server{
+		StartedAt: time.Now(),
+	}
+}
+
+// LoadEnv loads environment variables from the .env file and system environment variables.
+func (s *Server) LoadEnv() error {
+	viper.SetConfigFile(".env")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetLifeTime returns the duration of time elapsed since the server was started.
+func (s *Server) GetLifeTime() time.Duration {
+	return time.Since(s.StartedAt)
+}
